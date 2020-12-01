d4fd63931bc3474a8a2867f958a1d7f9704b3d60
Michelle Poole <michellepoole@Michelles-MacBook-Pro.local>
2020-12-01 09:58:58 -0800

    commented out landing page to test 404 page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LandingPage from './components/landing_page/landing_page';
+// import LandingPage from './components/landing_page/landing_page';
 import NotFound from './components/404';
 import './App.css';
 import './scss/custom.scss';
