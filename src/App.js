d5f51f61c8c7948149022bb56adef6bcffcb24d6
Michelle Poole <michellepoole@Michelles-MacBook-Pro.local>
2020-12-01 13:33:17 -0800

    created navbar js and scss files created basic navbar and added logo and icons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 // import LandingPage from './components/landing_page/landing_page';
 import NotFound from './components/404';
+import NavBar from './components/navbar/NavBar';
 import './App.css';
 import './scss/custom.scss';
 
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         {/* <LandingPage /> */}
         {/* eventually replace below route with landing page */}
+        <NavBar />
         <Route exact path="/" component={NotFound} />
         <Route component={NotFound} />
         <NotFound />
