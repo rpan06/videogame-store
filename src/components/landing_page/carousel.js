d96cb9b80f2b25e81f00fb6684525df230b60a6a
Krow Pan <krowpan@Krows-MacBook-Pro.local>
2020-11-30 22:12:39 -0800

    added landing page files and npm eslint, prettier

---
+++ src/components/landing_page/carousel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/* eslint-disable */
+
+export default class LandingPage extends React.Component {
+  state = {};
+
+  componentDidMount() {}
+
+  handleChange = (event) => {};
+  handleSubmit = (event) => {};
+
+  render() {
+    return <div className='center container'></div>;
+  }
+}
+
+/* eslint-enable */
---
+++ src/components/landing_page/landing_page.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Carousel from './carousel';
+import GameList from './game_list';
+import Ads from './ads';
+
+/* eslint-disable */
+
+export default class LandingPage extends React.Component {
+  state = {};
+
+  componentDidMount() {}
+
+  handleChange = (event) => {};
+
+  handleSubmit = (event) => {};
+
+  render() {
+    return (
+      <div className='container'>
+        <Carousel />
+        <GameList />
+        <Ads />
+      </div>
+    );
+  }
+}
+
+/* eslint-enable */
