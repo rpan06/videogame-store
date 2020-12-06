eeba1f7a279cd6eb2026dd5395d48b8c4c9b0f71
Michelle Poole <michellepoole@Michelles-MacBook-Pro.local>
2020-12-05 20:46:11 -0800

    added description and mobile responsiveness

--- src/components/individual_page/game_info/GameInfoPage.js
+++ src/components/individual_page/game_info/GameInfoPage.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import LoadingSpinner from '../../shared/loading_spinner';
 import TopInfo from './TopInfo';
 import '../../../scss/individual_page/individual_page_game_info.scss';
 
@@ -9,7 +8,7 @@ export default function GameInfoPage(props) {
   return (
     <Container className="pt-5 mt-3" id="game-info-container">
       <Row>
-        <Col lg={2} className="about-game">
+        <Col lg={2} className="about-game d-none d-lg-block">
           About Game
           </Col>
         <TopInfo game={props.game} />
