783a525028aaeeda5a52dcf7b3d0d5e6e1efe71a
Michelle Poole <michellepoole@Michelles-MacBook-Pro.local>
2020-12-17 14:36:40 -0800

    truncated game name to fix stacking

--- client/src/components/landing_page/game_list/game_item.js
+++ client/src/components/landing_page/game_list/game_item.js
@@ -7,13 +7,17 @@ export default (props) => {
   const { id, name, background_image: backgroundImage } = props.game;
   const price = CalculatePrice(id);
 
+  const truncate = (str) => {
+    return `${str.substring(0, 13)}...`;
+  };
+
   return (
     <Link to={`/game/${id}`}>
       <div className="game-slide">
         <img src={backgroundImage} alt="" />
         <div className="game-info">
           <div className="col-6 px-2">
-            <p className="game-title">{name}</p>
+            <p className="game-title">{truncate(name)}</p>
           </div>
           <div className="col-6 px-2">
             <p className="game-price">${price}</p>
